be22ac4d4cda150bee9fda7299709e327503a8dd
Chukharev Fedor <randomfedor@gmail.com>
2019-04-03 22:35:54 +0300

    Implement ObservableModelData

--- 6Sem_Lab2/ObservableModelData.cs
+++ 6Sem_Lab2/ObservableModelData.cs
@@ -9,5 +9,39 @@ namespace _6Sem_Lab2
 {
     class ObservableModelData : ObservableCollection<ModelData>
     {
+        private bool _hasChanged;
+
+        public bool HasChanged {
+            get => _hasChanged;
+            set => _hasChanged = value;
+        }
+
+        public void Add_ModelData(ModelData modelData) => Add(modelData);
+
+        public void Remove_At(int index) => RemoveAt(index);
+
+        public void AddDefaults()
+        {
+            Add_ModelData(new ModelData(1, 2));
+            Add_ModelData(new ModelData(3, 2));
+            Add_ModelData(new ModelData(24, 2));
+            Add_ModelData(new ModelData(24, 5));
+        }
+
+        public ModelData Farthest(int index) => (from item in Items
+                                                 orderby Math.Abs(item.Parameter - Items[index].Parameter) descending
+                                                 select item).First();
+
+
+        public override string ToString()
+        {
+            string result = "Total items: " + Count + "\n";
+            result += "Parameters of Data:\n";
+            foreach (var item in Items)
+            {
+                result += item.Parameter + "\n";
+            }
+            return result;
+        }
     }
 }
