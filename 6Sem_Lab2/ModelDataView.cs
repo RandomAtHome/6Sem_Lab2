d2153eba17b17f636d45952166dc224032625812
Chukharev Fedor <randomfedor@gmail.com>
2019-04-03 22:49:01 +0300

    Implement ModelDataView prototype

---
+++ 6Sem_Lab2/ModelDataView.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _6Sem_Lab2
+{
+    class ModelDataView
+    {
+        public ModelDataView(ObservableModelData modelDatas) {}
+
+        public void Draw(Chart chart, ModelData md1, ModelData md2)
+        {
+            // Do stuff
+        }
+    }
+}
